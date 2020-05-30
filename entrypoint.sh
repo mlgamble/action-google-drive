3b09d491512011032a16aec3ba1d9d1fa66a0c1a
mlgamble <mlgamble7@gmail.com>
2020-05-30 15:35:58 +1200

    Updated to accept generic action and args

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,15 +13,4 @@ if [ -n "$GOOGLE_CLIENT_ID" ]; then
     sed -i -e "s/;clientsecret=YOUR_GOOGLE_APP_SECRET/clientsecret=$GOOGLE_CLIENT_SECRET/" ~/.skicka.config
 fi
 
-skicka -no-browser-auth upload -ignore-times "$UPLOAD_FROM" "$UPLOAD_TO"
-
-# Remove outdated
-if [ $REMOVE_OUTDATED == "true" ]; then
-    skicka -verbose download -ignore-times "$UPLOAD_TO" "$UPLOAD_FROM" 2>&1 | \
-        sed "/Downloaded and wrote/!d" | \
-        sed -E "s/.*bytes to //" | \
-        xargs -I{} skicka rm "$UPLOAD_FROM{}" || true
-elif [ $REMOVE_OUTDATED != "false" ]; then
-    echo '$REMOVE_OUTDATED must be "true" or "false".'
-    exit 1
-fi
+skicka -verbose $ACTION $ARGS 2>&1
